1995c5f27150cdb2f1b31e92d0f76f1d678792fe
Monika Rani <monika6240@gmail.com>
2022-11-08 19:24:08 +0530

    add comment

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const API_URL = 'http://www.omdbapi.com?apikey=989a364d';
     "Type": "movie",
     "Poster": "https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg"
 }*/
+// Haha
 
 const App = () => {
     const [movies, setMovies] = useState([]);
